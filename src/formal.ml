db438e66ae9502f1a1343fb2acfa9736f245ead6
Samuel Mimram <smimram@gmail.com>
2022-04-27 16:58:23 +0200

    More formal.

--- src/formal.ml
+++ src/formal.ml
@@ -8,6 +8,8 @@ module T = struct
     | Pair : 'a t * 'b t -> ('a * 'b) t
     | Unit : unit t
 
+  type ex_t = Ex : 'a t -> ex_t
+
   (** Proofs of type equlaity. *)
   type (_, _) eq = Refl : ('a, 'a) eq
 
@@ -24,6 +26,9 @@ module T = struct
         | Some Refl, Some Refl -> Some Refl
         | _ -> None
       )
+
+  (** Whether two types are equal. *)
+  let eq : type a b . a t -> b t -> bool = fun x y -> decide x y <> None
 end
 
 (** Expressions. *)
@@ -38,6 +43,9 @@ module E = struct
     | Add : float t * float t -> float t
     | Pair : 'a t * 'b t -> ('a * 'b) t
     | Unit : unit t
+    | Ref : string * 'a T.t -> 'a ref t
+    | Get : 'a ref t -> 'a t
+    | Set : 'a ref t * 'a t -> unit t
 
   (** A typed formal expression. *)
   (* aka "existential wrapper" *)
@@ -46,8 +54,29 @@ module E = struct
   (** A typing environment. *)
   and environment = (string * tt) list
 
+  (** A state. *)
+  type state = (string * tt ref) list
+
+  (** Compute the value of an evaluated expression. *)
+  let rec value : type a . a t -> a = function
+    | Float x -> x
+    | Unit -> ()
+    | Var _ -> assert false
+    | Fun _ -> assert false
+    | App _ -> assert false
+    | Seq _ -> assert false
+    | Add _ -> assert false
+    | Ref _ -> assert false
+    | Get _ -> assert false
+    | Set _ -> assert false
+    | Pair (t, u) -> (value t, value u)
+
+  let is_value : type a . a t -> bool = fun x ->
+    try ignore (value x); true
+    with _ -> false
+
   (** Evaluate an expression. *)
-  let rec eval : type a . environment -> a t -> a t = fun env -> function
+  let rec eval : type a . state -> environment -> a t -> a t = fun state env -> function
     | Var (x,a) as var ->
       (
         match List.assoc_opt x env with
@@ -62,64 +91,100 @@ module E = struct
     | Fun (None, x, t) -> Fun (Some env, x, t)
     | Fun (Some env, x, t) -> Fun (Some env, x, t)
     | App (t, u) ->
-      let t = eval env t in
-      let u = eval env u in
+      let t = eval state env t in
+      let u = eval state env u in
       (
         match t with
-        | Fun (Some env, (x, a), t) -> eval ((x,TE(a,u))::env) t
+        | Fun (Some env, (x, a), t) -> eval state ((x,TE(a,u))::env) t
         | _ -> App (t, u)
       )
     | Float _ as t -> t
     | Seq (t, u) ->
-      let t = eval env t in
-      let u = eval env u in
+      let t = eval state env t in
+      let u = eval state env u in
       (
         match t with
         | Unit -> u
         | _ -> Seq (t, u)
       )
     | Add (t, u) ->
-      let t = eval env t in
-      let u = eval env u in
+      let t = eval state env t in
+      let u = eval state env u in
       (
         match t, u with
         | Float x1, Float x2 -> Float (x1 +. x2)
         (* TODO: simplifications such as adding 0. *)
         | _ -> Add (t, u)
       )
-    | Pair (t, u) -> Pair (eval env t, eval env u)
+    | Pair (t, u) -> Pair (eval state env t, eval state env u)
     | Unit -> Unit
-
-  (** Compute the value of an evaluated expression. *)
-  let rec value : type a . a t -> a = function
-    | Float x -> x
-    | Unit -> ()
-    | Var _ -> assert false
-    | Fun _ -> assert false
-    | App _ -> assert false
-    | Seq _ -> assert false
-    | Add _ -> assert false
-    | Pair (t, u) -> (value t, value u)
+    | Ref _ as t -> t
+    | Get r ->
+      let r = eval state env r in
+      (
+        match r with
+        | Ref (r',a) ->
+          (
+            match List.assoc_opt r' state with
+            | Some { contents = TE (a', t) } ->
+              (
+                match T.decide a a' with
+                | Some Refl -> t
+                | None -> assert false
+              )
+            | None -> Get r
+          )
+        | _ -> Get r
+      )
+    | Set (t, u) ->
+      let t = eval state env t in
+      let u = eval state env u in
+      (
+        match t with
+        | Ref (r',a) ->
+          (
+            match List.assoc_opt r' state with
+            | Some ({ contents = TE (a', _) } as r) ->
+              (
+                match T.decide a a' with
+                | Some Refl -> r := TE (a, u); Unit
+                | None -> assert false
+              )
+            | None -> Set (t, u)
+          )
+        | _ -> Set (t, u)
+      )
 end
 
 module Stream = struct
   open E
   
   (** A stream basically is an expression encoding a function which takes dt and
-      returns a float. *)
-  type 'a t = (float -> 'a) E.t
+      returns a float. We also provide the list of defined references along with
+      their type. *)
+  type 'a t = (float -> 'a) E.t * (string * T.ex_t) list
+
+  let expr : 'a t -> (float -> 'a) E.t = fst
+
+  let refs : 'a t -> (string * T.ex_t) list = snd
 
   let dt = "dt", T.Float
 
-  let value : 'a t -> 'a E.t = fun x -> App (x, Var dt)
+  (** Current value of a stream. *)
+  let value : 'a t -> 'a E.t = fun x -> App (expr x, Var dt)
 
-  let return : 'a E.t -> 'a t = fun x -> Fun (None, dt, x)
+  (** Return an expression as a constant stream. *)
+  let return ?(refs=[]) (x : 'a E.t) : 'a t = Fun (None, dt, x), refs
 
+  (** Constant float stream. *)
   let float x : float t = return (Float x)
 
-  let bind (f : 'a E.t -> 'b t) (x : 'a t) : 'b t = Fun (None, dt, App (f (value x), Var dt))
+  let bind (f : 'a E.t -> 'b t) (x : 'a t) : 'b t =
+    let y = f (value x) in
+    Fun (None, dt, value y), (refs x)@(refs y)
 
-  let prod : 'a t -> 'b t -> ('a * 'b) t = fun x y -> return (E.Pair (value x, value y))
+  let prod (x : 'a t) (y : 'b t) : ('a * 'b) t =
+    return ~refs:((refs x)@(refs y)) (E.Pair (value x, value y))
 
   module Operations = struct
     (** Return. *)
@@ -146,5 +211,5 @@ module Stream = struct
 
   include Operations
 
-  let dt : float t = Fun (None, dt, Var dt)
+  let dt : float t = Fun (None, dt, Var dt), []
 end
