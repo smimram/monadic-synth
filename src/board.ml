a063de068915619080cdac085b7ccccfe33746b7
Samuel Mimram <smimram@gmail.com>
2025-11-26 11:01:27 +0100

    Indentation.

--- src/board.ml
+++ src/board.ml
@@ -12,26 +12,26 @@ let create board =
     Graphics.set_color Graphics.cyan;
     List.iteri
       (fun j l ->
-         List.iteri
-           (fun i (label,c) ->
-              let x = dx * i in
-              let y = Graphics.size_y () - dy * j in
-              (* Draw label *)
-              let w,h = Graphics.text_size label in
-              Graphics.moveto (x+dx/2-w/2) (y-dx-h);
-              Graphics.draw_string label;
-              (* Draw control *)
-              match c with
-              | `Knob(min,max,mode,s) ->
-                Graphics.draw_circle (x+dx/2) (y-dx/2) 30;
-                let v = get s in
-                let v = Math.unstretch ~mode ~min ~max v in
-                let a = int_of_float (270. -. v *. 360.) in
-                Graphics.draw_arc (x+dx/2) (y-dx/2) 25 25 270 a;
-              | `Switch s ->
-                let r = 20 in
-                (if get s then Graphics.fill_rect else Graphics.draw_rect) (x+dx/2-r/2) (y-dx/2-r/2) r r
-           ) l
+        List.iteri
+          (fun i (label,c) ->
+            let x = dx * i in
+            let y = Graphics.size_y () - dy * j in
+            (* Draw label *)
+            let w,h = Graphics.text_size label in
+            Graphics.moveto (x+dx/2-w/2) (y-dx-h);
+            Graphics.draw_string label;
+            (* Draw control *)
+            match c with
+            | `Knob(min,max,mode,s) ->
+               Graphics.draw_circle (x+dx/2) (y-dx/2) 30;
+               let v = get s in
+               let v = Math.unstretch ~mode ~min ~max v in
+               let a = int_of_float (270. -. v *. 360.) in
+               Graphics.draw_arc (x+dx/2) (y-dx/2) 25 25 270 a;
+            | `Switch s ->
+               let r = 20 in
+               (if get s then Graphics.fill_rect else Graphics.draw_rect) (x+dx/2-r/2) (y-dx/2-r/2) r r
+          ) l
       ) board
   in
   draw ();
